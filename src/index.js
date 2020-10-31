604d63e076c054785e4a9b62da4f8a7b7298fb5c
Arif Bashar <arifbasharx@gmail.com>
2020-10-31 17:12:58 -0500

    modifying courses obj

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function getLabs() {
   // Keep track of the previous CRN
   let prevCRN = courses[0].CRN;
 
-  // Iterate through all the course objects in the
+  // Iterate through all the course objects in the courses sheet
   for (let i = 1; i < courses.length; i++) {
     /* If there is a duplicate course with the same CRN,
       then it is considered a lab */
@@ -35,7 +35,8 @@ function getLabs() {
         COURSE: courses[i].SUBJ + " " + courses[i].CRSE + " " + courses[i].SEC,
         TITLE: courses[i].TITLE,
         DAYS: getDays(courses[i]),
-        TIME: courses[i].BEGIN + " - " + courses[i].END_1,
+        BEGIN: parseInt(courses[i].BEGIN),
+        END: parseInt(courses[i].END_1),
         PROF: courses[i].FIRST + " " + courses[i].LAST,
       });
     }
@@ -80,48 +81,145 @@ function getGTAs() {
       if (cellAsString[0] === "A") {
         let value = scheduleSheet[cell].v;
         
-
-        // We know that student name cell always
-        // precedes the CRN: cell. If there is no student name
-        // before that cell, throw an error.
+        /* We know that student name cell always
+          precedes the "CRN:" cell. If there is no student name
+          before that cell, throw an error. */
         if (value.includes("CRN:")) {
-          if ((prevCellVal.includes(":")))
+          // Executes if previous cell value is not a student name
+          if (notStudentName(prevCellVal))
             throw new Error("There is a schedule with no student name");
-          else
-            console.log(prevCellVal);
+          else {
+            //
+            gta.Student = prevCellVal;
+            gtas.push(gta);
+            gta = {};
+          }
         }
       }
-
     }
     prevCellVal = scheduleSheet[cell].v;
+  }
+
+  appendDays(scheduleSheet, gtas);
+  appendTime(scheduleSheet, gtas);
+
+
+  return gtas;
+}
+
+function appendDays(scheduleSheet, gtas) {
+  let busy;
+  let sched;
+  let index = -1;
+
+  for (let cell in scheduleSheet) {
+    const cellAsString = cell.toString();
+
+    if (
+      cellAsString[1] !== "r" &&
+      cellAsString[1] !== "m" &&
+      cellAsString[1] > 0
+    ) {
+      if (cellAsString[0] === "H") {
+        let value = scheduleSheet[cell].v;
+        
+        if (value === "DAYS" && index < gtas.length) {
+          busy = [];
+          sched = {};
+          index++
+        } else {
+          sched = {
+            Days: value,
+            Time: ''
+          }
+          busy.push(sched);
+          gtas[index].Busy = busy;
+
+        }
+      }
+    }
+  }
+}
+
+function appendTime(scheduleSheet, gtas) {
+  let busy;
+  let sched;
+  let index = -1;
+  let busyIndex = 0;
+
+  for (let cell in scheduleSheet) {
+    const cellAsString = cell.toString();
+
+    if (
+      cellAsString[1] !== "r" &&
+      cellAsString[1] !== "m" &&
+      cellAsString[1] > 0
+    ) {
+      if (cellAsString[0] === "I") {
+        let value = scheduleSheet[cell].v;
+        
+        if (value === "TIME" && index < gtas.length) {
+          // console.log(busyIndex);
+          busy = [];
+          sched = {};
+          busyIndex = 0;
+          index++;
+        } else {
+          // if (busyIndex === gtas[index].Busy.length) {
+          //   busyIndex = 0;
+          //   console.log("length is", gtas[index].Busy.length)
+
+          // }
+          if (value.includes("AM") || value.includes("PM")) {
+            gtas[index].Busy[busyIndex].Time = value;
+            // console.log(index, busyIndex);
+            // console.log(gtas[index].Busy);
+            busyIndex++;
+          }
+
+        }
+      }
+    }
   }
 }
 
 // Concatenates all the day properties in each lab object
 function getDays(lab) {
-  let days = "";
+  let days = {
+    M: false,
+    T: false,
+    W: false,
+    TR: false,
+    F: false
+  }
 
   // Checks if these keys are in the object
-  if ("M" in lab) days += "M ";
+  if ("M" in lab) days.M = true;
 
-  if ("T" in lab) days += "T ";
+  if ("T" in lab) days.T = true;
 
-  if ("W" in lab) days += "W ";
+  if ("W" in lab) days.W = true;
 
-  if ("TR" in lab) days += "TR ";
+  if ("TR" in lab) days.TR = true;
 
-  if ("F" in lab) days += "F ";
+  if ("F" in lab) days.F = true;
 
   // Remove the extra whitespace at the end
-  days = days.slice(0, -1);
+  // days = days.slice(0, -1);
 
   return days;
 }
 
+function notStudentName(cellValue) {
+  return cellValue.includes(":");
+}
+
 function main() {
   let labs = getLabs();
-  getGTAs();
-  // console.log(labs);
+  let gtas = getGTAs();
+  console.log(labs);
+  console.log(gtas);
+
 }
 
 main();
